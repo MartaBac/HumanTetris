6ba4a140efcac2eaa2e1e832571d30b385f58de0
Nico Michelis <mikomiki@hotmail.it>
2017-03-31 17:14:52 +0200

    Initial

---
+++ resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by resource.rc
+//
+#define IDI_MYICON                      104
+#define IDI_CHILDICON                   105
+#define CM_FILE_NEW                     9070
+#define CM_FILE_EXIT                    9071
+#define CM_FILE_OPEN                    9072
+#define CM_FILE_SAVE                    9073
+#define CM_FILE_SAVEAS                  9074
+#define CM_EDIT_UNDO                    9075
+#define CM_EDIT_CUT                     9076
+#define CM_EDIT_COPY                    9077
+#define CM_EDIT_PASTE                   9078
+#define CM_WINDOW_CASCADE               9079
+#define CM_WINDOW_ARRANGE               9080
+#define CM_WINDOW_TILEVERT              9081
+#define CM_WINDOW_TILEHORZ              9082
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
